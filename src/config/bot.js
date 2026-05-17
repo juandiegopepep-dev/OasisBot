958c44882a03cfd653a9e2e25dd11565616678b9
juandiegopepep-dev <juandiegopepep@gmail.com>
2026-05-17 15:07:21 -0400

    Updatebot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Pium Pium!! cuidando a los de oasis",
         // Activity type number (0 = Playing).
         type: 0, 
       },
